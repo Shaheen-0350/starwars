18c9b2c2d8b2ef2c4626929165239ff5d126da61
Shaheen-0350 <134725071+Shaheen-0350@users.noreply.github.com>
2024-07-18 16:27:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.alert('Use desktop mode and landscape mode when using mobile')
+const userInput = document.getElementById('userInput');
+const characterImage = document.getElementById('characterImage');
+const Name = document.getElementById('name');
+const gender = document.getElementById('gender');
+const species = document.getElementById('species');
+
+function find() {
+    fetch("https://akabab.github.io/starwars-api/api/all.json").then(response => response.json()).then(data => {
+        console.log(data)
+        characterImage.setAttribute('src', data[userInput.value-1].image);
+        Name.innerHTML='Name:  '+(data[userInput.value-1].name).toUpperCase();
+        gender.innerHTML='Gender:  '+(data[userInput.value-1].gender).toUpperCase();
+        species.innerHTML='Species:  '+(data[userInput.value-1].species).toUpperCase();
+    });
+    }
