f030801f96c986820d623685650abcab1900b1f4
Shaheen-0350 <134725071+Shaheen-0350@users.noreply.github.com>
2024-07-18 16:32:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const species = document.getElementById('species');
 
 function find() {
     fetch("https://akabab.github.io/starwars-api/api/all.json").then(response => response.json()).then(data => {
-        console.log(data)
         characterImage.setAttribute('src', data[userInput.value-1].image);
         Name.innerHTML='Name:  '+(data[userInput.value-1].name).toUpperCase();
         gender.innerHTML='Gender:  '+(data[userInput.value-1].gender).toUpperCase();
